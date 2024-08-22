a9d88874c7ae5f251f4ec470384b92a4aeb72624
cranckv3 <Ankitshresthaas5@gmail.com>
2024-08-22 18:17:16 +0545

    water : Add Vendor / Vendor-ims /sepolicy

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,8 @@
 # Vendor
-https://github.com/cranckv3/vendor_xaomi_water.git vendor/xiaomi/water
+git clone https://github.com/cranckv3/vendor_xaomi_water.git vendor/xiaomi/water
 
 # Vendor-ims
-https://github.com/cranckv3/proprietary_vendor_xiaomi_ims-water.git  vendor/xiaomi/ims-water
+git clone https://github.com/cranckv3/proprietary_vendor_xiaomi_ims-water.git  vendor/xiaomi/ims-water
+
+# Sepolicy
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
