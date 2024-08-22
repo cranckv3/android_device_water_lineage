8229ffd501da1dc68341785b63d3777dd36159ec
cranckv3 <Ankitshresthaas5@gmail.com>
2024-08-22 18:06:09 +0545

    water : Add Vendor / Vendor-ims

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Vendor
+https://github.com/cranckv3/vendor_xaomi_water.git vendor/xiaomi/water
+
+# Vendor-ims
+https://github.com/cranckv3/proprietary_vendor_xiaomi_ims-water.git  vendor/xiaomi/ims-water
